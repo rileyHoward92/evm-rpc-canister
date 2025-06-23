rileyHoward92/evm-rpc-canister

commit 502c086ae95a03fd1b8ae0555073c766182fddd8

feat: support for `eth_call` (#308)
Add a convenience method, similar to the other existing `eth_.*`
methods, to call `eth_call` via multiple providers and aggregate theirs
answers based on the provided consensus strategy, allowing callers to
read data from Ethereum smart contracts.

src/candid_rpc/mod.rs
@@ -158,6 +158,18 @@ impl CandidRpcClient {
         )
         .map(|result| from_send_raw_transaction_result(transaction_hash.clone(), result))
     }
+
+    pub async fn eth_call(
+        &self,
+        args: evm_rpc_types::CallArgs,
+    ) -> MultiRpcResult<evm_rpc_types::Hex> {
+        use crate::candid_rpc::cketh_conversion::{from_data, into_eth_call_params};
+        process_result(
+            RpcMethod::EthFeeHistory,
+            self.client.eth_call(into_eth_call_params(args)).await,
+        )
+        .map(from_data)
+    }
 }
 
 fn get_transaction_hash(raw_signed_transaction_hex: &Hex) -> Option<Hex32> {

evm_rpc_types/src/request/mod.rs
@@ -1,4 +1,4 @@
-use crate::{Hex20, Hex32, Nat256};
+use crate::{Hex, Hex20, Hex32, HexByte, Nat256};
 use candid::CandidType;
 use serde::Deserialize;
 
@@ -56,4 +56,82 @@ pub struct GetLogsArgs {
 pub struct GetTransactionCountArgs {
     pub address: Hex20,
     pub block: BlockTag,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, CandidType, Deserialize)]
+pub struct CallArgs {
+    pub transaction: TransactionRequest,
+    /// Integer block number, or "latest" for the last mined block or "pending", "earliest" for not yet mined transactions.
+    /// Default to "latest" if unspecified, see https://github.com/ethereum/execution-apis/issues/461.
+    pub block: Option<BlockTag>,
+}
+
+#[derive(Clone, Debug, Default, PartialEq, Eq, CandidType, Deserialize)]
+pub struct TransactionRequest {
+    /// The type of the transaction:
+    /// - "0x0" for legacy transactions (pre- EIP-2718)
+    /// - "0x1" for access list transactions (EIP-2930)
+    /// - "0x2" for EIP-1559 transactions
+    #[serde(rename = "type")]
+    pub tx_type: Option<HexByte>,
+
+    /// Transaction nonce
+    pub nonce: Option<Nat256>,
+
+    /// Address of the receiver or `None` in a contract creation transaction.
+    pub to: Option<Hex20>,
+
+    /// The address of the sender.
+    pub from: Option<Hex20>,
+
+    /// Gas limit for the transaction.
+    pub gas: Option<Nat256>,
+
+    /// Amount of ETH sent with this transaction.
+    pub value: Option<Nat256>,
+
+    /// Transaction input data
+    pub input: Option<Hex>,
+
+    /// The legacy gas price willing to be paid by the sender in wei.
+    #[serde(rename = "gasPrice")]
+    pub gas_price: Option<Nat256>,
+
+    /// Maximum fee per gas the sender is willing to pay to miners in wei.
+    #[serde(rename = "maxPriorityFeePerGas")]
+    pub max_priority_fee_per_gas: Option<Nat256>,
+
+    /// The maximum total fee per gas the sender is willing to pay (includes the network / base fee and miner / priority fee) in wei.
+    #[serde(rename = "maxFeePerGas")]
+    pub max_fee_per_gas: Option<Nat256>,
+
+    /// The maximum total fee per gas the sender is willing to pay for blob gas in wei.
+    #[serde(rename = "maxFeePerBlobGas")]
+    pub max_fee_per_blob_gas: Option<Nat256>,
+
+    /// EIP-2930 access list
+    #[serde(rename = "accessList")]
+    pub access_list: Option<AccessList>,
+
+    /// List of versioned blob hashes associated with the transaction's EIP-4844 data blobs.
+    #[serde(rename = "blobVersionedHashes")]
+    pub blob_versioned_hashes: Option<Vec<Hex32>>,
+
+    /// Raw blob data.
+    pub blobs: Option<Vec<Hex>>,
+
+    /// Chain ID that this transaction is valid on.
+    #[serde(rename = "chainId")]
+    pub chain_id: Option<Nat256>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, CandidType, Deserialize)]
+#[serde(transparent)]
+pub struct AccessList(pub Vec<AccessListEntry>);
+
+#[derive(Clone, Debug, PartialEq, Eq, CandidType, Deserialize)]
+pub struct AccessListEntry {
+    pub address: Hex20,
+    #[serde(rename = "storageKeys")]
+    pub storage_keys: Vec<Hex32>,
 }

src/rpc_client/json/mod.rs
@@ -7,6 +7,8 @@ use std::fmt::{Debug, Display, Formatter, LowerHex, UpperHex};
 
 pub mod requests;
 pub mod responses;
+#[cfg(test)]
+mod tests;
 
 macro_rules! bytes_array {
     ($name: ident, $size: expr) => {
@@ -74,6 +76,7 @@ macro_rules! bytes_array {
 bytes_array!(FixedSizeData, 32);
 bytes_array!(Hash, 32);
 bytes_array!(LogsBloom, 256);
+bytes_array!(StorageKey, 32);
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(transparent)]

src/rpc_client/mod.rs
@@ -14,6 +14,8 @@ use json::Hash;
 use serde::{de::DeserializeOwned, Serialize};
 use std::collections::{BTreeMap, BTreeSet};
 use std::fmt::Debug;
+use crate::rpc_client::json::requests::EthCallParams;
+use crate::rpc_client::json::responses::Data;
 
 pub mod amount;
 pub(crate) mod eth_rpc;
@@ -395,6 +397,16 @@ impl EthRpcClient {
             "eth_getTransactionCount",
             params,
             self.response_size_estimate(50 + HEADER_SIZE_LIMIT),
+        )
+        .await
+        .reduce(self.consensus_strategy())
+    }
+    
+    pub async fn eth_call(&self, params: EthCallParams) -> Result<Data, MultiCallError<Data>> {
+        self.parallel_call(
+            "eth_call",
+            params,
+            self.response_size_estimate(256 + HEADER_SIZE_LIMIT),
         )
         .await
         .reduce(self.consensus_strategy())

src/rpc_client/numeric/mod.rs
@@ -17,6 +17,9 @@ pub enum TransactionCountTag {}
 /// but depending on the block height the two may differ.
 pub type TransactionCount = Amount<TransactionCountTag>;
 
+pub enum TransactionNonceTag {}
+pub type TransactionNonce = Amount<TransactionNonceTag>;
+
 pub enum TransactionIndexTag {}
 pub type TransactionIndex = Amount<TransactionIndexTag>;
 
@@ -44,3 +47,6 @@ pub type NumBytes = Amount<NumBytesTag>;
 
 pub enum TimestampTag {}
 pub type Timestamp = Amount<TimestampTag>;
+
+pub enum ChainIdTag {}
+pub type ChainId = Amount<ChainIdTag>;

candid/evm_rpc.did
@@ -69,6 +69,31 @@ type GetLogsArgs = record {
   topics : opt vec Topic;
 };
 type GetTransactionCountArgs = record { address : text; block : BlockTag };
+type CallArgs = record {
+  transaction : TransactionRequest;
+  block : opt BlockTag;
+};
+type TransactionRequest = record {
+  "type" : opt text;
+  nonce : opt nat;
+  to : opt text;
+  from : opt text;
+  gas : opt nat;
+  value : opt nat;
+  input : opt text;
+  gasPrice : opt nat;
+  maxPriorityFeePerGas : opt nat;
+  maxFeePerGas : opt nat;
+  maxFeePerBlobGas : opt nat;
+  accessList: opt vec AccessListEntry;
+  blobVersionedHashes : opt vec text;
+  blobs : opt vec text;
+  chainId : opt nat;
+};
+type AccessListEntry = record {
+  address : text;
+  storageKeys : vec text;
+};
 type HttpHeader = record { value : text; name : text };
 type HttpOutcallError = variant {
   IcError : record { code : RejectionCode; message : text };
@@ -136,6 +161,10 @@ type MultiSendRawTransactionResult = variant {
   Consistent : SendRawTransactionResult;
   Inconsistent : vec record { RpcService; SendRawTransactionResult };
 };
+type MultiCallResult = variant {
+  Consistent : CallResult;
+  Inconsistent : vec record { RpcService; CallResult };
+};
 type ProviderError = variant {
   TooFewCycles : record { expected : nat; received : nat };
   MissingRequiredProvider;
@@ -185,6 +214,7 @@ type SendRawTransactionResult = variant {
   Ok : SendRawTransactionStatus;
   Err : RpcError;
 };
+type CallResult = variant { Ok : text; Err : RpcError };
 type RequestResult = variant { Ok : text; Err : RpcError };
 type RequestCostResult = variant { Ok : nat; Err : RpcError };
 type RpcConfig = record { responseSizeEstimate : opt nat64; responseConsensus : opt ConsensusStrategy };
@@ -259,6 +289,7 @@ service : (InstallArgs) -> {
   eth_getTransactionCount : (RpcServices, opt RpcConfig, GetTransactionCountArgs) -> (MultiGetTransactionCountResult);
   eth_getTransactionReceipt : (RpcServices, opt RpcConfig, hash : text) -> (MultiGetTransactionReceiptResult);
   eth_sendRawTransaction : (RpcServices, opt RpcConfig, rawSignedTransactionHex : text) -> (MultiSendRawTransactionResult);
+  eth_call : (RpcServices, opt RpcConfig, CallArgs) -> (MultiCallResult);
   request : (RpcService, json : text, maxResponseBytes : nat64) -> (RequestResult);
   requestCost : (RpcService, json : text, maxResponseBytes : nat64) -> (RequestCostResult) query;
   getMetrics : () -> (Metrics) query;

evm_rpc_types/src/lib.rs
@@ -16,7 +16,10 @@ use std::fmt::{Debug, Display, Formatter};
 use std::str::FromStr;
 
 pub use lifecycle::{InstallArgs, LogFilter, RegexString};
-pub use request::{BlockTag, FeeHistoryArgs, GetLogsArgs, GetTransactionCountArgs};
+pub use request::{
+    AccessList, AccessListEntry, BlockTag, CallArgs, FeeHistoryArgs, GetLogsArgs,
+    GetTransactionCountArgs, TransactionRequest,
+};
 pub use response::{Block, FeeHistory, LogEntry, SendRawTransactionStatus, TransactionReceipt};
 pub use result::{
     HttpOutcallError, JsonRpcError, MultiRpcResult, ProviderError, RpcError, RpcResult,
@@ -240,5 +243,11 @@ impl From<u8> for Byte {
 impl From<u8> for HexByte {
     fn from(value: u8) -> Self {
         Self(Byte::from(value))
+    }
+}
+
+impl From<HexByte> for u8 {
+    fn from(value: HexByte) -> Self {
+        value.0.into_byte()
     }
 }

scripts/examples
@@ -4,20 +4,22 @@
 NETWORK=local
 IDENTITY=default
 CANISTER_ID=evm_rpc
-CYCLES=1000000000
+CYCLES=10000000000
 WALLET=$(dfx identity get-wallet --network=$NETWORK --identity=$IDENTITY)
 RPC_SERVICE="EthMainnet=variant {PublicNode}"
 RPC_SERVICES=EthMainnet
-RPC_CONFIG=null
+RPC_CONFIG="opt record {responseConsensus = opt variant {Threshold = record {total = opt (3 : nat8); min = 2 : nat8}}}"
+# Use concrete block height to avoid flakiness on CI
+BLOCK_HEIGHT="Number = 20000000"
 
 FLAGS="--network=$NETWORK --identity=$IDENTITY --with-cycles=$CYCLES --wallet=$WALLET"
 
-
 dfx canister call $CANISTER_ID request "(variant {$RPC_SERVICE}, "'"{ \"jsonrpc\": \"2.0\", \"method\": \"eth_gasPrice\", \"params\": [], \"id\": 1 }"'", 1000)" $FLAGS || exit 1
 
-dfx canister call $CANISTER_ID eth_getLogs "(variant {$RPC_SERVICES}, $RPC_CONFIG, record {addresses = vec {\"0xdAC17F958D2ee523a2206206994597C13D831ec7\"}})" $FLAGS || exit 1
-dfx canister call $CANISTER_ID eth_getBlockByNumber "(variant {$RPC_SERVICES}, $RPC_CONFIG, variant {Latest})" $FLAGS || exit 1
+dfx canister call $CANISTER_ID eth_getLogs "(variant {$RPC_SERVICES}, $RPC_CONFIG, record {fromBlock = opt variant {$BLOCK_HEIGHT}; toBlock = opt variant {$BLOCK_HEIGHT}; addresses = vec {\"0xdAC17F958D2ee523a2206206994597C13D831ec7\"}})" $FLAGS || exit 1
+dfx canister call $CANISTER_ID eth_getBlockByNumber "(variant {$RPC_SERVICES}, $RPC_CONFIG, variant {$BLOCK_HEIGHT})" $FLAGS || exit 1
 dfx canister call $CANISTER_ID eth_getTransactionReceipt "(variant {$RPC_SERVICES}, $RPC_CONFIG, \"0xdd5d4b18923d7aae953c7996d791118102e889bea37b48a651157a4890e4746f\")" $FLAGS || exit 1
-dfx canister call $CANISTER_ID eth_getTransactionCount "(variant {$RPC_SERVICES}, $RPC_CONFIG, record {address = \"0xdAC17F958D2ee523a2206206994597C13D831ec7\"; block = variant {Latest}})" $FLAGS || exit 1
-dfx canister call $CANISTER_ID eth_feeHistory "(variant {$RPC_SERVICES}, $RPC_CONFIG, record {blockCount = 3; newestBlock = variant {Latest}})" $FLAGS || exit 1
+dfx canister call $CANISTER_ID eth_getTransactionCount "(variant {$RPC_SERVICES}, $RPC_CONFIG, record {address = \"0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045\"; block = variant {$BLOCK_HEIGHT}})" $FLAGS || exit 1
+dfx canister call $CANISTER_ID eth_feeHistory "(variant {$RPC_SERVICES}, $RPC_CONFIG, record {blockCount = 3; newestBlock = variant {$BLOCK_HEIGHT}})" $FLAGS || exit 1
 dfx canister call $CANISTER_ID eth_sendRawTransaction "(variant {$RPC_SERVICES}, $RPC_CONFIG, \"0xf86c098504a817c800825208943535353535353535353535353535353535353535880de0b6b3a76400008025a028ef61340bd939bc2195fe537567866003e1a15d3c71ff63e1590620aa636276a067cbe9d8997f761aecb703304b3800ccf555c9f3dc64214b297fb1966a3b6d83\")" $FLAGS || exit 1
+dfx canister call $CANISTER_ID eth_call "(variant {$RPC_SERVICES}, $RPC_CONFIG, record {transaction = record {to = opt \"0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48\"; input = opt \"0x70a08231000000000000000000000000b25eA1D493B49a1DeD42aC5B1208cC618f9A9B80\"}; block = opt variant {$BLOCK_HEIGHT}})" $FLAGS || exit 1

commit 1e3a28799006378e773a8d94f4d32d8cc7a4fd20

fix: metric name for `eth_call`  (#439)
Fix a bug introduced in #308, where the metric name for the method
`eth_call` was the same as for `eth_feeHistory`.

src/candid_rpc/mod.rs
@@ -166,7 +166,7 @@ impl CandidRpcClient {
     ) -> MultiRpcResult<evm_rpc_types::Hex> {
         use crate::candid_rpc::cketh_conversion::{from_data, into_eth_call_params};
         process_result(
-            RpcMethod::EthFeeHistory,
+            RpcMethod::EthCall,
             self.client.eth_call(into_eth_call_params(args)).await,
         )
         .map(from_data)

src/types.rs
@@ -167,6 +167,7 @@ pub struct Metrics {
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum RpcMethod {
+    EthCall,
     EthFeeHistory,
     EthGetLogs,
     EthGetBlockByNumber,
@@ -178,6 +179,7 @@ pub enum RpcMethod {
 impl RpcMethod {
     fn name(self) -> &'static str {
         match self {
+            RpcMethod::EthCall => "eth_call",
             RpcMethod::EthFeeHistory => "eth_feeHistory",
             RpcMethod::EthGetLogs => "eth_getLogs",
             RpcMethod::EthGetBlockByNumber => "eth_getBlockByNumber",